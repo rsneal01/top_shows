b6db3e4460b84b48c0a4a6d61a9fbe2e25a36003
Bobby Neal <rsneal01@gmail.com>
2020-08-04 17:14:06 +0000

    modified NOTES

--- NOTES.md
+++ NOTES.md
@@ -19,3 +19,12 @@ Show a list of high rated TV shows
 3. Show 3
 
 user types a number to receive show description and tomatoe score of that list item
+
+what is a show?
+a show has a name
+a show has a critic score
+a show has a certified_fresh
+a show has a URL
+  one level deeper:
+  a show has a description
+  a show has an audience score
--- lib/top_shows/cli.rb
+++ lib/top_shows/cli.rb
@@ -5,6 +5,7 @@ class TopShows::CLI
   def call 
     list_shows
     more_info
+    goodbye
   end
   
   def list_shows
@@ -14,17 +15,25 @@ class TopShows::CLI
   end
   
   def more_info
-    puts "Enter list number for more info about that show or type exit:"
     input = nil
     while input != "exit"
+    puts "Enter list number for more info about that show, type list for all shows, or type exit:"
       input = gets.strip.downcase
       case input
       when "1"
         puts "Description for show #1"
       when "2"
         puts "Description for show #2"
+      when "list"
+        list_shows
+      else 
+        puts "Unknown command, please type valid show number, list, or exit"
       end
     end
+  end
+  
+  def goodbye
+    puts "Goodbye"
   end
   
 end
