f3dc59676772218e9cda4a3592e59ee617e2b86c
Bobby Neal <rsneal01@gmail.com>
2020-08-06 16:39:54 +0000

    modified TopShows:Shows

--- lib/top_shows/shows.rb
+++ lib/top_shows/shows.rb
@@ -3,17 +3,23 @@ class TopShows::Shows
   
   attr_accessor :name, :critic_score, :certified_fresh, :url
   
-  def self.all
-    # returns all instances of shows
-    # puts "Top rated TV shows on rotten tomoatoes:"
-    # puts "1. The Umbrella Academy: Season 1"
-    # puts "2. P-Valley: Season 1" 
+  def self.all_shows
+    self.scrape_shows
+  end
+  
+  def self.scrape_shows
+    shows = []
+    shows << self.scrape_rt
+  end
     
-    show_1 = self.new
-    show_1.name = "The Umbrella Academy: Season 1"
-    show_1.critic_score = ""
-    show_1.certified_fresh = ""
-    show_1.url = ""
+  def self.scrape_rt
+    doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/top-tv"))
+    
+    show = self.new
+    show.name = doc.search()
+    show.critic_score = 
+    show.certified_fresh = 
+    show.url = 
     
     show_2 = self.new
     show_2.name = "P-Valley: Season 1"
