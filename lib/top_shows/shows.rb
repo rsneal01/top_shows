f7e5aa33ed862627f00f07ffce6ee891dde72ccb
Bobby Neal <rsneal01@gmail.com>
2020-08-07 17:35:16 +0000

    modified TopShows:Shows scraper

--- lib/top_shows/shows.rb
+++ lib/top_shows/shows.rb
@@ -15,21 +15,32 @@ class TopShows::Shows
     
   def self.scrape_imdb
     doc = Nokogiri::HTML(open("https://www.imdb.com/chart/toptv?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=4da9d9a5-d299-43f2-9c53-f0efa18182cd&pf_rd_r=3J64KKA7W3EK66X8KA27&pf_rd_s=right-4&pf_rd_t=15506&pf_rd_i=tvmeter&ref_=chttvm_ql_6"))
+    show_title = doc.css("td.titleColumn")
+    show_score = dosc.css("")
+    
+    # assign each show attributes individually?
     
     show_1 = self.new
-    show_1.name = doc.css("td.titleColumn").first.text
+    show_1.name = show_title.first.text
     # should return: 1. Planet Earth II (2016)
     show_1.critic_score = 
     show_1.
     show_1.url = 
     
-    show_2 = self.new
-    show_2.name = "P-Valley: Season 1"
-    show_2.critic_score = ""
-    show_2. = ""
-    show_2.url = ""
+    [show_1, show_2, show_3, show_4, show_5]
     
-    [show_1, show_2]
+    
+    # or assign with iteration?
+    
+    scraped_shows_imbd = []
+    scraped_show = self.new
+    # should this instantiation go insdide our iteration?
+    top_five = show_title.first(5)
+    top_five.each do |show|
+      scraped_show.name = show.text
+      scraped_shows_imbd << show
+    end
+    scraped_shows_imbd
   end
   
 end
